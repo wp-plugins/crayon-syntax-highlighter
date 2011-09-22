14e538e55a7e484d2555e060d929815bf9c3283b
akarmenia <akarmenia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 15:07:59 +0000

    = 1.0.3 = * Added highlight="false" attribute to temporarily disable highlighting. * Fixed default color of font for twilight font.
    git-svn-id: https://plugins.svn.wordpress.org/crayon-syntax-highlighter/tags/1.0.3@442194 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ crayon_themes.class.php
@@ -0,0 +1,25 @@
+<?php
+require_once ('global.php');
+require_once (CRAYON_RESOURCE_PHP);
+
+/* Manages themes once they are loaded. */
+class CrayonThemes extends CrayonUsedResourceCollection {
+	// Properties and Constants ===============================================
+
+	const DEFAULT_THEME = 'classic';
+	const DEFAULT_THEME_NAME = 'Classic';
+
+	// Methods ================================================================
+
+	function __construct() {
+		$this->directory ( CRAYON_THEME_PATH );
+		$this->set_default ( self::DEFAULT_THEME, self::DEFAULT_THEME_NAME );
+	}
+
+	// XXX Override
+
+	public function path($id) {
+		return CRAYON_THEME_PATH . $id . crayon_slash () . "$id.css";
+	}
+}
+?>

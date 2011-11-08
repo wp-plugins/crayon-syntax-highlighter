a68cbc5defcd7810b4a477bfd76d07f44dced0a8
akarmenia <akarmenia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-08 04:10:49 +0000

    = 1.5.0 = * Added ability to cache remote code requests for a set period of time to reduce server load. See Settings > Crayon > Misc. You can clear the cache at any time in settings. Set the cache clearing interval to "Immediately" to prevent caching. * Fixed a bug preventing dropdown settings from being set correctly * Fixed AJAX settings bug * Fixed CSS syntax bug for fonts * Improved code popup, strips style atts * Added preview code for shell, renamed to 'Shell' * Code popup window now shows either highlighted or plain code, depending on which is currently visible
    git-svn-id: https://plugins.svn.wordpress.org/crayon-syntax-highlighter/tags/1.5.0@460456 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ crayon_themes.class.php
@@ -0,0 +1,43 @@
+<?php
+require_once ('global.php');
+require_once (CRAYON_RESOURCE_PHP);
+
+/* Manages themes once they are loaded. */
+class CrayonThemes extends CrayonUsedResourceCollection {
+	// Properties and Constants ===============================================
+
+	const DEFAULT_THEME = 'classic';
+	const DEFAULT_THEME_NAME = 'Classic';
+
+	private $printed_themes = array();
+	
+	// Methods ================================================================
+
+	function __construct() {
+		$this->directory ( CRAYON_THEME_PATH );
+		$this->set_default ( self::DEFAULT_THEME, self::DEFAULT_THEME_NAME );
+	}
+
+	// XXX Override
+
+	public function path($id) {
+		return CRAYON_THEME_PATH . $id . "/$id.css";
+	}
+	
+	// Prints out CSS for all the used themes
+	public function get_used_theme_css() {
+		global $CRAYON_VERSION;
+		$used = $this->get_used();
+		$css = '';
+		foreach ($used as $theme) {
+			if (!in_array($theme, $this->printed_themes)) {
+				$url = CrayonGlobalSettings::plugin_path() . CrayonUtil::pathf(CRAYON_THEME_DIR) . $theme->id() . '/' . $theme->id() . '.css?ver' . $CRAYON_VERSION;
+				$css .= '<link rel="stylesheet" type="text/css" href="' . $url . '" />' . CRAYON_NL;
+				$this->printed_themes[] = $theme;
+			}
+		}
+		return $css;
+	}
+	
+}
+?>

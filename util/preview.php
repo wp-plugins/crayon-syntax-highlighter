ee7a0ee59811f3afe5c7adfcf9949d106e3570ab
akarmenia <akarmenia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-07 07:21:47 +0000

    = 1.7.11 = * Added the option of either enqueuing themes and fonts (efficient) or printing them before the Crayon each time when enqueuing fails * Thanks to http://www.adostudio.it/ for finding the bugs * Improved theme and font handling * Improved theme detection and Crayon capturing
    git-svn-id: https://plugins.svn.wordpress.org/crayon-syntax-highlighter/tags/1.7.11@486033 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ util/preview.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once (dirname(dirname(__FILE__)) . '/crayon_wp.class.php');
+
+$wp_root_path = str_replace('wp-content/plugins/' . CRAYON_DIR, '', CrayonUtil::pathf(CRAYON_ROOT_PATH));
+require_once ($wp_root_path . 'wp-load.php');
+
+echo '<link rel="stylesheet" href="', plugins_url(CRAYON_STYLE, dirname(__FILE__)),
+	'?ver=', $CRAYON_VERSION, '" type="text/css" media="all" />';
+echo '<script type="text/javascript">
+
+jQuery(document).ready(function() {
+    //CrayonSyntax.init();
+});
+
+</script>';
+echo '<div id="content">';
+CrayonSettingsWP::load_settings(); // Run first to ensure global settings loaded
+
+$crayon = CrayonWP::instance();
+
+// Load settings from GET and validate
+foreach ($_GET as $key => $value) {
+	//echo $key, ' ', $value , '<br/>';
+	$_GET[$key] = CrayonSettings::validate($key, $value);
+}
+$crayon->settings($_GET);
+$settings = array(CrayonSettings::TOP_SET => TRUE, CrayonSettings::TOP_MARGIN => 10, 
+		CrayonSettings::BOTTOM_SET => FALSE, CrayonSettings::BOTTOM_MARGIN => 0);
+$crayon->settings($settings);
+
+// Print the theme CSS
+$theme_id = $crayon->setting_val(CrayonSettings::THEME);
+echo CrayonResources::themes()->get_css($theme_id);
+
+$font_id = $crayon->setting_val(CrayonSettings::FONT);
+echo CrayonResources::fonts()->get_css($font_id);
+
+// Load custom code based on language
+$lang = $crayon->setting_val(CrayonSettings::FALLBACK_LANG);
+$path = crayon_pf( dirname(__FILE__) . '/sample/' . $lang . '.txt', FALSE );
+
+if ($lang && @file_exists($path)) {
+	$crayon->url($path);
+} else {
+	$code = "
+// A sample class
+class Human {
+	private int age = 0;
+	public void birthday() {
+		age++;
+		print('Happy Birthday!');
+	}
+}
+";
+	$crayon->code($code);
+}
+$crayon->title('Sample Code');
+$crayon->marked('5-7');
+$crayon->output($highlight = true, $nums = true, $print = true);
+echo '</div>';
+CrayonWP::load_textdomain();
+printf(crayon__('Change the %1$sfallback language%2$s to change the sample code. Lines 5-7 are marked.'), '<a href="#langs">', '</a>');
+
+?>
